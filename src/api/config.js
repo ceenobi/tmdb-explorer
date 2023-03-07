91c6d55358351b0eb9c8638900a590dcac9fcacc
ceenobi <cobimbachu@gmail.com>
2023-03-07 22:24:10 +0100

    bg color

--- src/api/config.js
+++ src/api/config.js
@@ -3,7 +3,6 @@ export const BASE_URL = 'https://api.themoviedb.org/3'
 export const API_KEY = import.meta.env.VITE_TMDB_API_KEY
 
 export const options = {
-  // qs: { page: '1' },
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
