e0aca3b4f50055d0d6b4a31b13000e89ec8d042d
ceenobi <cobimbachu@gmail.com>
2023-03-13 01:09:46 +0100

    fix bgcolor

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -14,7 +14,7 @@ export default function Layout() {
             </p>
           </div>
         </div>
-        <div className='sideA'>
+        <div className='sideA bg-dark'>
           <Outlet />
         </div>
       </div>
